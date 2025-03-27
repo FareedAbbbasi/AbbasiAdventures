58d17bb28ba00e5e8c0d64c40e6126e15e515345
FareedAbbbasi <codingabbas0999@gmail.com>
2025-03-27 10:01:18 +0500

    chore: Initial commit of travel website project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='w-full'>
+                <div className='bg-img'>
+                    <div className='w-[1278px] h-[67.18px] '>
+                        <img src="/src/assets/logo/logo.png" alt="" />
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Services</li>
+                            <li>Upcoming pakages</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
